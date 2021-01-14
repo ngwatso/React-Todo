d3c88c0933bea0e5c19ceed1e5db34800f722ede
ngwatso <ngwatson79@yahoo.com>
2021-01-13 23:16:26 -0500

    initial commit

---
+++ src/components/TodoForm.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+class TodoForm extends Component {
+	constructor() {
+		super();
+		this.state = {
+			itemName: "",
+		};
+	}
+
+	// ! update state with each keystroke
+	handleChanges = (e) => {
+		this.setState({ itemName: e.target.value });
+	};
+
+	// ! submit form
+	handleSubmit = (e) => {
+		e.preventDefault();
+		this.props.addItem(this.state.itemName);
+	};
+
+	// ! render form
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<input
+					onChange={this.handleChanges}
+					value={this.state.itemName}
+					type="text"
+					name="item"
+				/>
+				<button>Add List Item</button>
+			</form>
+		);
+	}
+}
